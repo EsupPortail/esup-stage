18fa8a22af1b0d3551d88542c0c42b0b36335326
Joshua Baubry <joshua.baubry@gmail.com>
2020-12-03 13:43:54 +0100

    hello world with primefaces + springboot

---
+++ src/main/java/fr/esupportail/esupstage/services/beans/HelloWorldBean.java
@@ -0,0 +1,31 @@
+package fr.esupportail.esupstage.services.beans;
+
+import javax.annotation.ManagedBean;
+import javax.inject.Named;
+
+@Named(value="helloWorld")
+public class HelloWorldBean {
+
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String showGreeting() {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
+}
