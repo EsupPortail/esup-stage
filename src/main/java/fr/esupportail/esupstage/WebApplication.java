542d2d949fb144bbad323563bb686fb8429cff34
Joshua Baubry <joshua.baubry@gmail.com>
2020-12-03 15:10:13 +0100

    remove useless imports

--- src/main/java/fr/esupportail/esupstage/WebApplication.java
+++ src/main/java/fr/esupportail/esupstage/WebApplication.java
@@ -5,20 +5,12 @@
  */
 package fr.esupportail.esupstage;
 
+import fr.esupportail.esupstage.property.ApplicationProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.security.servlet.SecurityAutoConfiguration;
-import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
-import org.springframework.boot.web.servlet.ServletRegistrationBean;
 import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
-
-import fr.esupportail.esupstage.property.ApplicationProperties;
-import org.springframework.context.annotation.Bean;
-
-import javax.faces.webapp.FacesServlet;
-import javax.servlet.ServletContext;
-import java.util.Arrays;
 
 // @EnableCaching
 @SpringBootApplication(exclude = { SecurityAutoConfiguration.class })
