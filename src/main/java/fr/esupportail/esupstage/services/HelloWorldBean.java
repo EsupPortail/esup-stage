ad2f61f5111a38695c8dbb44fa5ccbabe395b787
Joshua Baubry <joshua.baubry@gmail.com>
2020-12-04 09:41:31 +0100

    hello world bean example

---
+++ src/main/java/fr/esupportail/esupstage/services/HelloWorldBean.java
@@ -0,0 +1,30 @@
+package fr.esupportail.esupstage.services;
+
+import javax.inject.Named;
+
+@Named(value="helloWorld")
+public class HelloWorldBean {
+
+    private String firstName = "John";
+    private String lastName = "Doe";
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String showGreeting() {
+        return "Hello " + firstName + " " + lastName + "!";
+    }
+}
