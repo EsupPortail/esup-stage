5eddac7d55962130e269fa906821db2516ef6b39
shiipou <shiishii@nocturlab.fr>
2021-01-29 11:11:52 +0100

    Fix tests

--- src/test/java/fr/esupportail/esupstage/domain/jpa/FAP_QualificationSimplifieeRepositoryTest.java
+++ src/test/java/fr/esupportail/esupstage/domain/jpa/FAP_QualificationSimplifieeRepositoryTest.java
@@ -132,9 +132,7 @@ class FAP_QualificationSimplifieeRepositoryTest extends AbstractTest {
 		entityManager.persist(typeStructure);
 
 		final Structure structure = new Structure();
-		structure.setDateCreation(new Date());
 		structure.setEstValidee(1);
-		structure.setLoginCreation("login");
 		structure.setRaisonSociale("raison");
 		structure.setVoie("voie");
 		structure.setEffectif(effectifStructure);
