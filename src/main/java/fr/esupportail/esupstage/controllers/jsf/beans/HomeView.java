ea93e38d2bd7af00d8b3b4f66c2dc890a9695b26
shiipou <shiishii@nocturlab.fr>
2021-02-10 15:39:24 +0100

    Change template links

--- src/main/java/fr/esupportail/esupstage/controllers/jsf/beans/HomeView.java
+++ src/main/java/fr/esupportail/esupstage/controllers/jsf/beans/HomeView.java
@@ -1,11 +1,5 @@
 package fr.esupportail.esupstage.controllers.jsf.beans;
 
-import org.primefaces.model.menu.DefaultMenuItem;
-import org.primefaces.model.menu.DefaultMenuModel;
-import org.primefaces.model.menu.DefaultSubMenu;
-import org.primefaces.model.menu.MenuModel;
-
-import javax.annotation.PostConstruct;
 import javax.enterprise.context.RequestScoped;
 import javax.inject.Named;
 
