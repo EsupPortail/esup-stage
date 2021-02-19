95a4ceb5ae9901fe3b2db49a67df330a3173c8cd
Vianney Dubus <vianney.dubus@gmail.com>
2021-02-19 12:21:34 +0100

    Clean up imports

--- src/main/java/fr/esupportail/esupstage/domain/jpa/entities/NafN1.java
+++ src/main/java/fr/esupportail/esupstage/domain/jpa/entities/NafN1.java
@@ -9,6 +9,16 @@ import javax.persistence.Id;
 import javax.persistence.OneToMany;
 import javax.persistence.Table;
 
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.NamedQuery;
+import javax.persistence.OneToMany;
+import javax.persistence.Table;
+
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
