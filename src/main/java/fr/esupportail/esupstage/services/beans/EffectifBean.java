beb36d9f28f5b4b3cfb9b609c22cece63edddc10
Joshua Baubry <joshua.baubry@gmail.com>
2021-03-19 14:53:00 +0100

    remove some jpa annotation

--- src/main/java/fr/esupportail/esupstage/services/beans/EffectifBean.java
+++ src/main/java/fr/esupportail/esupstage/services/beans/EffectifBean.java
@@ -13,31 +13,10 @@ import java.util.List;
 @NoArgsConstructor
 public class EffectifBean {
 		private static final long serialVersionUID = 1L;
-		@Id
-		@GeneratedValue(strategy = GenerationType.AUTO)
-		@Column(unique = true, nullable = false)
 		private Integer idEffectif;
-		@Column(nullable = false, length = 100)
 		private String libelleEffectif;
 		private boolean modifiable;
-		@Column(nullable = false, length = 1)
 		private String temEnServEffectif;
-		// bi-directional many-to-one association to Structure
-		@OneToMany(mappedBy = "effectif")
-		private List<Structure> structures;
-
-		public Structure addStructure(Structure structure) {
-				getStructures().add(structure);
-		//		structure.setEffectif(this);
-				return structure;
-		}
-
-		public Structure removeStructure(Structure structure) {
-				getStructures().remove(structure);
-				structure.setEffectif(null);
-				return structure;
-		}
-
 		@Override public String toString() {
 				return this.libelleEffectif;
 		}
