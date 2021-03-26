9406621a76f527eb55944f618e362dd7dad480f9
Joshua Baubry <joshua.baubry@gmail.com>
2021-03-26 14:48:59 +0100

    Reformat Avenant code

--- src/main/java/fr/esupportail/esupstage/controllers/jsf/beans/HelloWorldBean.java
+++ src/main/java/fr/esupportail/esupstage/controllers/jsf/beans/HelloWorldBean.java
@@ -2,29 +2,28 @@ package fr.esupportail.esupstage.controllers.jsf.beans;
 
 import javax.inject.Named;
 
-@Named(value = "helloWorld")
-public class HelloWorldBean {
+@Named(value = "helloWorld") public class HelloWorldBean {
 
-	private String firstName = "John";
-	private String lastName = "Doe";
+		private String firstName = "John";
+		private String lastName = "Doe";
 
-	public String getFirstName() {
-		return firstName;
-	}
+		public String getFirstName() {
+				return firstName;
+		}
 
-	public void setFirstName(String firstName) {
-		this.firstName = firstName;
-	}
+		public void setFirstName(String firstName) {
+				this.firstName = firstName;
+		}
 
-	public String getLastName() {
-		return lastName;
-	}
+		public String getLastName() {
+				return lastName;
+		}
 
-	public void setLastName(String lastName) {
-		this.lastName = lastName;
-	}
+		public void setLastName(String lastName) {
+				this.lastName = lastName;
+		}
 
-	public String showGreeting() {
-		return "Hello " + firstName + " " + lastName + "!";
-	}
+		public String showGreeting() {
+				return "Hello " + firstName + " " + lastName + "!";
+		}
 }
