dc6d6e864f45c34707a83373c8c289f2222f9c08
Jean-Christophe Gay <jean-christophe.gay@dauphine.psl.eu>
2021-08-24 10:43:47 +0200

    déployement en mode war

--- src/main/java/fr/dauphine/estage/EstageApplication.java
+++ src/main/java/fr/dauphine/estage/EstageApplication.java
@@ -2,9 +2,10 @@ package fr.dauphine.estage;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class EstageApplication {
+public class EstageApplication extends SpringBootServletInitializer {
 
 	public static void main(String[] args) {
 		SpringApplication.run(EstageApplication.class, args);
