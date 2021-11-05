7167e70728cd65728486a2e00146d05b89c0f556
tjubera <tjubera@osaxis.fr>
2021-11-05 17:43:50 +0100

    ESTAGE-10 Création/modification convention - création du webservice d'update via DTO

--- src/main/java/org/esup_portail/esup_stage/controller/ConventionController.java
+++ src/main/java/org/esup_portail/esup_stage/controller/ConventionController.java
@@ -3,12 +3,12 @@ package org.esup_portail.esup_stage.controller;
 import com.fasterxml.jackson.annotation.JsonView;
 import org.esup_portail.esup_stage.dto.ContextDto;
 import org.esup_portail.esup_stage.dto.PaginatedResponse;
+import org.esup_portail.esup_stage.dto.ConventionFormDto;
 import org.esup_portail.esup_stage.dto.view.Views;
 import org.esup_portail.esup_stage.enums.AppFonctionEnum;
 import org.esup_portail.esup_stage.enums.DroitEnum;
-import org.esup_portail.esup_stage.model.Convention;
-import org.esup_portail.esup_stage.model.Role;
-import org.esup_portail.esup_stage.model.Utilisateur;
+import org.esup_portail.esup_stage.exception.AppException;
+import org.esup_portail.esup_stage.model.*;
 import org.esup_portail.esup_stage.model.helper.UtilisateurHelper;
 import org.esup_portail.esup_stage.repository.ConventionJpaRepository;
 import org.esup_portail.esup_stage.repository.ConventionRepository;
@@ -16,11 +16,11 @@ import org.esup_portail.esup_stage.security.ServiceContext;
 import org.esup_portail.esup_stage.security.interceptor.Secure;
 import org.json.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
 
 import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -76,5 +76,22 @@ public class ConventionController {
             convention = new Convention();
         }
         return convention;
+    }
+
+
+    @PutMapping("/{id}")
+    @Secure(fonction = AppFonctionEnum.ORGA_ACC, droits = {DroitEnum.MODIFICATION})
+    public Convention update(@PathVariable("id") int id, @Valid @RequestBody ConventionFormDto conventionFormDto) {
+        Convention convention = conventionJpaRepository.findById(id);
+        setConventionData(convention, conventionFormDto);
+        convention = conventionJpaRepository.saveAndFlush(convention);
+        return convention;
+    }
+
+    private void setConventionData(Convention convention, ConventionFormDto conventionFormDto) {
+        if (convention == null) {
+            throw new AppException(HttpStatus.NOT_FOUND, "Convention non trouvée");
+        }
+        //TODO set champs
     }
 }
---
+++ src/main/java/org/esup_portail/esup_stage/dto/ConventionFormDto.java
@@ -0,0 +1,244 @@
+package org.esup_portail.esup_stage.dto;
+
+import org.esup_portail.esup_stage.enums.NbJoursHebdoEnum;
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.math.BigDecimal;
+import java.util.Date;
+
+public class ConventionFormDto {
+
+    @NotNull
+    private int idEtudiant;
+
+    @NotNull
+    private int idCentreGestion;
+
+    @NotNull
+    private int codeUFR;
+
+    @NotNull
+    private int codeEtape;
+
+    @Size(max = 10)
+    private String codeDepartement;
+
+    @NotNull
+    private int idEnseignant;
+
+    @NotNull
+    private int idStructure;
+
+    @NotNull
+    private int idService;
+
+    @NotNull
+    private int idContact;
+
+    @NotNull
+    private int idSignataire;
+
+    @NotNull
+    private int idTypeConvention;
+
+    @NotNull
+    private int idOffre;
+
+    @NotNull
+    @NotEmpty
+    private String sujetStage;
+
+    @NotNull
+    @NotEmpty
+    private Date dateDebutStage;
+
+    @NotNull
+    @NotEmpty
+    private Date dateFinStage;
+
+    @NotNull
+    private Boolean interruptionStage;
+
+    private Date dateDebutInterruption;
+
+    private Date dateFinInterruption;
+
+    @NotNull
+    private NbJoursHebdoEnum nbJoursHebdo;
+
+    @NotNull
+    private int idTempsTravail;
+
+    private String commentaireDureeTravail;
+
+    @NotNull
+    private int codeLangueConvention;
+
+    @NotNull
+    private int idOrigineStage;
+
+    @NotNull
+    private int idTheme;
+
+    private Boolean conventionStructure;
+
+    private Boolean validationPedagogique;
+
+    private Boolean validationConvention;
+
+    private Boolean conversionEnContrat;
+
+    private String commentaireStage;
+
+    @Size(max = 200)
+    private String adresseEtudiant;
+
+    @Size(max = 10)
+    private String codePostalEtudiant;
+
+    @Size(max = 80)
+    private String villeEtudiant;
+
+    @Size(max = 50)
+    private String paysEtudiant;
+
+    @Size(max = 100)
+    private String courrielPersoEtudiant;
+
+    @Size(max = 20)
+    private String telEtudiant;
+
+    @Size(max = 20)
+    private String telPortableEtudiant;
+
+    @NotNull
+    private int idIndemnisation;
+
+    @Size(max = 15)
+    private String montantGratification;
+
+    private String fonctionsEtTaches;
+
+    private String details;
+
+    @Size(max = 10)
+    private String annee;
+
+    @NotNull
+    private int idAssurance;
+
+    @Size(max = 15)
+    private String insee;
+
+    @Size(max = 5)
+    private String codeCaisse;
+
+    @Size(max = 5)
+    private String nbHeuresHebdo;
+
+    private Integer quotiteTravail;
+
+    private String modeEncadreSuivi;
+
+    @NotNull
+    private int idModeVersGratification;
+
+    private String avantagesNature;
+
+    @NotNull
+    private int idNatureTravail;
+
+    @NotNull
+    private int idModeValidationStage;
+
+    @Size(max = 8)
+    private String codeElp;
+
+    @Size(max = 60)
+    private String libelleELP;
+
+    private BigDecimal creditECTS;
+
+    private String travailNuitFerie;
+
+    private Integer dureeStage;
+
+    @Size(max = 100)
+    private String nomEtabRef;
+
+    @Size(max = 200)
+    private String adresseEtabRef;
+
+    @Size(max = 30)
+    private String nomSignataireComposante;
+
+    @Size(max = 60)
+    private String qualiteSignataire;
+
+    @Size(max = 100)
+    private String libelleCPAM;
+
+    @Size(max = 4)
+    private String dureeExceptionnelle;
+
+    @NotNull
+    private int idUniteDureeExceptionnelle;
+
+    @NotNull
+    private int idUniteGratification;
+
+    @Size(max = 3)
+    private String codeFinalite;
+
+    @Size(max = 60)
+    private String libelleFinalite;
+
+    @Size(max = 1)
+    private String codeCursusLMD;
+
+    private Boolean priseEnChargeFraisMission;
+
+    @Size(max = 1)
+    private String codeRGI;
+
+    @Size(max = 50)
+    private String loginValidation;
+
+    private Date dateValidation;
+
+    @Size(max = 50)
+    private String loginSignature;
+
+    private Date dateSignature;
+
+    private Boolean envoiMailEtudiant;
+
+    private Date dateEnvoiMailEtudiant;
+
+    private Boolean envoiMailTuteurPedago;
+
+    private Date dateEnvoiMailTuteurPedago;
+
+    private Boolean envoiMailTuteurPro;
+
+    private Date dateEnvoiMailTuteurPro;
+
+    private String nbConges;
+    private String competences;
+
+    @NotNull
+    private int idUniteDureeGratification;
+
+    @Size(max = 50)
+    private String monnaieGratification;
+
+    @Size(max = 10)
+    private String volumeHoraireFormation;
+
+    @Size(max = 30)
+    private String typePresence;
+
+    @NotNull
+    private int idDevise;
+}
