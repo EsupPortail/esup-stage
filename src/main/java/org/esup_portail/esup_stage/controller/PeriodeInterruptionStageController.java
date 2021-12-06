e0ce1b776b9cf22602b24d4226dab08d8a37fdfe
tjubera <tjubera@osaxis.fr>
2021-12-06 10:45:02 +0100

    ESTAGE-25 module Calendrier - Ajout du backend pour PeriodeInterruptionStage

---
+++ src/main/java/org/esup_portail/esup_stage/controller/PeriodeInterruptionStageController.java
@@ -0,0 +1,105 @@
+package org.esup_portail.esup_stage.controller;
+
+import org.esup_portail.esup_stage.dto.PaginatedResponse;
+import org.esup_portail.esup_stage.dto.PeriodeInterruptionStageDto;
+import org.esup_portail.esup_stage.enums.AppFonctionEnum;
+import org.esup_portail.esup_stage.enums.DroitEnum;
+import org.esup_portail.esup_stage.exception.AppException;
+import org.esup_portail.esup_stage.model.Convention;
+import org.esup_portail.esup_stage.model.PeriodeInterruptionStage;
+import org.esup_portail.esup_stage.repository.ConventionJpaRepository;
+import org.esup_portail.esup_stage.repository.PeriodeInterruptionStageJpaRepository;
+import org.esup_portail.esup_stage.repository.PeriodeInterruptionStageRepository;
+import org.esup_portail.esup_stage.security.interceptor.Secure;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.*;
+
+import javax.servlet.http.HttpServletResponse;
+import javax.validation.Valid;
+import java.util.List;
+
+@ApiController
+@RequestMapping("/periode-interruption-stage")
+public class PeriodeInterruptionStageController {
+
+    @Autowired
+    PeriodeInterruptionStageRepository periodeInterruptionStageRepository;
+
+    @Autowired
+    PeriodeInterruptionStageJpaRepository periodeInterruptionStageJpaRepository;
+
+    @Autowired
+    ConventionJpaRepository conventionJpaRepository;
+
+    @GetMapping
+    @Secure(fonctions = {AppFonctionEnum.NOMENCLATURE}, droits = {DroitEnum.LECTURE})
+    public PaginatedResponse<PeriodeInterruptionStage> search(@RequestParam(name = "page", defaultValue = "1") int page, @RequestParam(name = "perPage", defaultValue = "50") int perPage, @RequestParam("predicate") String predicate, @RequestParam(name = "sortOrder", defaultValue = "asc") String sortOrder, @RequestParam(name = "filters", defaultValue = "{}") String filters, HttpServletResponse response) {
+        PaginatedResponse<PeriodeInterruptionStage> paginatedResponse = new PaginatedResponse<>();
+        paginatedResponse.setTotal(periodeInterruptionStageRepository.count(filters));
+        paginatedResponse.setData(periodeInterruptionStageRepository.findPaginated(page, perPage, predicate, sortOrder, filters));
+        return paginatedResponse;
+    }
+
+    @GetMapping("/{id}")
+    @Secure(fonctions = {AppFonctionEnum.ORGA_ACC}, droits = {DroitEnum.LECTURE})
+    public PeriodeInterruptionStage getById(@PathVariable("id") int id) {
+        PeriodeInterruptionStage periodeInterruptionStage = periodeInterruptionStageJpaRepository.findById(id);
+        if (periodeInterruptionStage == null) {
+            throw new AppException(HttpStatus.NOT_FOUND, "PeriodeInterruptionStage non trouvée");
+        }
+        return periodeInterruptionStage;
+    }
+
+    @GetMapping("/getByConvention/{id}")
+    @Secure(fonctions = {AppFonctionEnum.ORGA_ACC}, droits = {DroitEnum.LECTURE})
+    public List<PeriodeInterruptionStage> getByConvention(@PathVariable("id") int id) {
+        List<PeriodeInterruptionStage> periodeInterruptionStage = periodeInterruptionStageJpaRepository.findByConvention(id);
+        if (periodeInterruptionStage == null) {
+            throw new AppException(HttpStatus.NOT_FOUND, "PeriodeInterruptionStage non trouvée");
+        }
+        return periodeInterruptionStage;
+    }
+
+    @PostMapping
+    @Secure(fonctions = {AppFonctionEnum.ORGA_ACC}, droits = {DroitEnum.CREATION})
+    public PeriodeInterruptionStage create(@Valid @RequestBody PeriodeInterruptionStageDto periodeInterruptionStageDto) {
+        PeriodeInterruptionStage periodeInterruptionStage = new PeriodeInterruptionStage();
+        setPeriodeInterruptionStageData(periodeInterruptionStage, periodeInterruptionStageDto);
+        return periodeInterruptionStageJpaRepository.saveAndFlush(periodeInterruptionStage);
+    }
+
+    @PutMapping("/{id}")
+    @Secure(fonctions = {AppFonctionEnum.ORGA_ACC}, droits = {DroitEnum.MODIFICATION})
+    public PeriodeInterruptionStage update(@PathVariable("id") int id, @Valid @RequestBody PeriodeInterruptionStageDto periodeInterruptionStageDto) {
+        PeriodeInterruptionStage periodeInterruptionStage = periodeInterruptionStageJpaRepository.findById(id);
+        if (periodeInterruptionStage == null) {
+            throw new AppException(HttpStatus.NOT_FOUND, "PeriodeInterruptionStage non trouvée");
+        }
+        setPeriodeInterruptionStageData(periodeInterruptionStage, periodeInterruptionStageDto);
+        return periodeInterruptionStageJpaRepository.saveAndFlush(periodeInterruptionStage);
+    }
+
+    @DeleteMapping("/{id}")
+    @Secure(fonctions = {AppFonctionEnum.PARAM_GLOBAL}, droits = {DroitEnum.SUPPRESSION})
+    public boolean delete(@PathVariable("id") int id) {
+        PeriodeInterruptionStage periodeInterruptionStage = periodeInterruptionStageJpaRepository.findById(id);
+        if (periodeInterruptionStage == null) {
+            throw new AppException(HttpStatus.NOT_FOUND, "PeriodeInterruptionStage non trouvé");
+        }
+        periodeInterruptionStageJpaRepository.delete(periodeInterruptionStage);
+        periodeInterruptionStageJpaRepository.flush();
+        return true;
+    }
+
+    private void setPeriodeInterruptionStageData(PeriodeInterruptionStage periodeInterruptionStage, PeriodeInterruptionStageDto periodeInterruptionStageDto) {
+
+        Convention convention = conventionJpaRepository.findById(periodeInterruptionStageDto.getIdConvention());
+        if (periodeInterruptionStage == null) {
+            throw new AppException(HttpStatus.NOT_FOUND, "Convention non trouvée");
+        }
+        periodeInterruptionStage.setDateDebutInterruption(periodeInterruptionStageDto.getDateDebutInterruption());
+        periodeInterruptionStage.setDateFinInterruption(periodeInterruptionStageDto.getDateFinInterruption());
+        periodeInterruptionStage.setConvention(convention);
+    }
+}
---
+++ src/main/java/org/esup_portail/esup_stage/dto/PeriodeInterruptionStageDto.java
@@ -0,0 +1,40 @@
+package org.esup_portail.esup_stage.dto;
+
+import javax.validation.constraints.NotNull;
+import java.util.Date;
+
+public class PeriodeInterruptionStageDto {
+
+    @NotNull
+    private Date dateDebutInterruption;
+
+    @NotNull
+    private Date dateFinInterruption;
+
+    @NotNull
+    private int idConvention;
+
+    public Date getDateDebutInterruption() {
+        return dateDebutInterruption;
+    }
+
+    public void setDateDebutInterruption(Date dateDebutInterruption) {
+        this.dateDebutInterruption = dateDebutInterruption;
+    }
+
+    public Date getDateFinInterruption() {
+        return dateFinInterruption;
+    }
+
+    public void setDateFinInterruption(Date dateFinInterruption) {
+        this.dateFinInterruption = dateFinInterruption;
+    }
+
+    public int getIdConvention() {
+        return idConvention;
+    }
+
+    public void setIdConvention(int idConvention) {
+        this.idConvention = idConvention;
+    }
+}
