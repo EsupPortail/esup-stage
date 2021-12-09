995e9c7e5f096726685c6631181c1719fb61a589
tjubera <tjubera@osaxis.fr>
2021-12-09 15:18:20 +0100

    ESTAGE-25 module Calendrier - affichage des interruptions multiples crées et modifiées dans le récapitulatif de l'avenant

--- src/frontend/src/app/components/convention/avenant/avenant-view/avenant-view.component.ts
+++ src/frontend/src/app/components/convention/avenant/avenant-view/avenant-view.component.ts
@@ -2,6 +2,8 @@ import { Component, Output, EventEmitter, OnInit, Input } from '@angular/core';
 import { AuthService } from "../../../../services/auth.service";
 import { MessageService } from "../../../../services/message.service";
 import { AvenantService } from "../../../../services/avenant.service";
+import { PeriodeInterruptionStageService } from "../../../../services/periode-interruption-stage.service";
+import { PeriodeInterruptionAvenantService } from "../../../../services/periode-interruption-avenant.service";
 
 @Component({
   selector: 'app-avenant-view',
@@ -10,6 +12,10 @@ import { AvenantService } from "../../../../services/avenant.service";
 })
 export class AvenantViewComponent implements OnInit {
 
+  interruptionsStage: any[] = [];
+  addedInterruptionsStage: any[] = [];
+  modifiedInterruptionsStage: any[] = [];
+
   @Input() avenant: any;
   @Input() convention: any;
   @Output() updated = new EventEmitter<any>();
@@ -17,9 +23,12 @@ export class AvenantViewComponent implements OnInit {
   constructor(private authService: AuthService,
               private messageService: MessageService,
               private avenantService: AvenantService,
+              private periodeInterruptionStageService: PeriodeInterruptionStageService,
+              private periodeInterruptionAvenantService: PeriodeInterruptionAvenantService,
   ) { }
 
   ngOnInit(): void {
+    this.loadInterruptionsStage();
   }
 
   isEtudiant(): boolean {
@@ -35,6 +44,27 @@ export class AvenantViewComponent implements OnInit {
       this.avenant = response;
       this.messageService.setSuccess('Validation de l\'avenant annulée avec succès');
       this.updated.emit();
+    });
+  }
+
+
+  loadInterruptionsStage() : void{
+    this.periodeInterruptionStageService.getByConvention(this.convention.id).subscribe((response: any) => {
+      this.interruptionsStage = response;
+      this.loadInterruptionsAvenant();
+    });
+  }
+
+  loadInterruptionsAvenant() : void {
+    this.periodeInterruptionAvenantService.getByAvenant(this.avenant.id).subscribe((response: any) => {
+      for(let interruption of response){
+        if (interruption.isModif){
+          console.log('interruption : ' + JSON.stringify(interruption, null, 2));
+          this.modifiedInterruptionsStage.push(interruption);
+        }else{
+          this.addedInterruptionsStage.push(interruption);
+        }
+      }
     });
   }
 }
