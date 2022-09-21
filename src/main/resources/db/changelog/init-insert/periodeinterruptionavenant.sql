d71b59d575421db8812e23e2b18d6209928343d9
ssound <ssound@osaxis.fr>
2022-09-21 12:03:15 +0200

    periodes d'interruption - fix script d'init en ne prenant que les interruptions ayant une date de début et de fin

--- src/main/resources/db/changelog/init-insert/periodeinterruptionavenant.sql
+++ src/main/resources/db/changelog/init-insert/periodeinterruptionavenant.sql
@@ -1,4 +1,4 @@
 INSERT INTO `PeriodeInterruptionAvenant` (`idAvenant`, `dateDebutInterruption`, `dateFinInterruption`, `isModif`)
 SELECT `idAvenant`, `dateDebutInterruption`, `dateFinInterruption`, false
 FROM `Avenant`
-WHERE `interruptionStage` = 1;
+WHERE `interruptionStage` = 1 AND `dateDebutInterruption` IS NOT NULL AND `dateFinInterruption` IS NOT NULL;
--- src/main/resources/db/changelog/init-insert/periodeinterruptionstage.sql
+++ src/main/resources/db/changelog/init-insert/periodeinterruptionstage.sql
@@ -1,4 +1,4 @@
 INSERT INTO `PeriodeInterruptionStage` (`idConvention`, `dateDebutInterruption`, `dateFinInterruption`)
 SELECT `idConvention`, `dateDebutInterruption`, `dateFinInterruption`
 FROM `Convention`
-WHERE `interruptionStage` = 1;
+WHERE `interruptionStage` = 1 AND `dateDebutInterruption` IS NOT NULL AND `dateFinInterruption` IS NOT NULL;
