6a2975f9c16900f4cc7edef493d1ac8470da3e47
ssound <ssound@osaxis.fr>
2022-10-12 14:05:43 +0200

    ESTAGE-99 tableau de bord / filtre étape - recherche également sur le code

--- src/main/java/org/esup_portail/esup_stage/repository/EtapeRepository.java
+++ src/main/java/org/esup_portail/esup_stage/repository/EtapeRepository.java
@@ -1,10 +1,13 @@
 package org.esup_portail.esup_stage.repository;
 
 import org.esup_portail.esup_stage.model.Etape;
+import org.json.JSONObject;
 import org.springframework.stereotype.Repository;
 
 import javax.persistence.EntityManager;
+import javax.persistence.Query;
 import java.util.Arrays;
+import java.util.List;
 
 @Repository
 public class EtapeRepository extends PaginationRepository<Etape> {
@@ -12,5 +15,19 @@ public class EtapeRepository extends PaginationRepository<Etape> {
     public EtapeRepository(EntityManager em) {
         super(em, Etape.class, "e");
         this.predicateWhitelist = Arrays.asList("id", "libelle");
+    }
+
+    @Override
+    protected void addSpecificParameter(String key, JSONObject parameter, List<String> clauses) {
+        if (key.equals("search")) {
+            clauses.add("(e.libelle LIKE :search OR CONCAT(e.id.code, '-', e.id.codeVersionEtape) LIKE :search)");
+        }
+    }
+
+    @Override
+    protected void setSpecificParameterValue(String key, JSONObject parameter, Query query) {
+        if (key.equals("search")) {
+            query.setParameter(key.replace(".", ""), "%" + parameter.getString("value") + "%");
+        }
     }
 }
