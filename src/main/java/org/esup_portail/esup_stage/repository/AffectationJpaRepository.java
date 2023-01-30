a61e5b1aa53eb47133acb1dfa225fcbd7ab5ba13
ssound <ssound@osaxis.fr>
2023-01-30 10:24:15 +0100

    ESTAGE-329 affectation - création d'une ligne contenant uniquement le code université paramétré dans les paramétrages généraux

---
+++ src/main/java/org/esup_portail/esup_stage/repository/AffectationJpaRepository.java
@@ -0,0 +1,14 @@
+package org.esup_portail.esup_stage.repository;
+
+import org.esup_portail.esup_stage.model.Affectation;
+import org.esup_portail.esup_stage.model.AffectationId;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+import java.util.List;
+
+public interface AffectationJpaRepository extends JpaRepository<Affectation, AffectationId> {
+
+    @Query("SELECT a FROM Affectation a WHERE a.id.codeUniversite = :codeUniversite")
+    List<Affectation> findByCodeUniversite(String codeUniversite);
+}
