c72790ce4c7bbc13c1f581d8af60af2ab3e3b63f
Sylvain Kieffer <sylvain.kieffer@univ-paris13.fr>
2024-10-04 11:29:22 +0200

    Gestion des réponses CAS XML #184

--- src/main/java/org/esup_portail/esup_stage/config/SecurityConfiguration.java
+++ src/main/java/org/esup_portail/esup_stage/config/SecurityConfiguration.java
@@ -4,6 +4,7 @@ import org.esup_portail.esup_stage.bootstrap.ApplicationBootstrap;
 import org.esup_portail.esup_stage.security.userdetails.CasUserDetailsServiceImpl;
 import org.jasig.cas.client.session.SingleSignOutFilter;
 import org.jasig.cas.client.validation.TicketValidator;
+import org.jasig.cas.client.validation.Cas20ServiceTicketValidator;
 import org.jasig.cas.client.validation.json.Cas30JsonServiceTicketValidator;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -49,6 +50,10 @@ public class SecurityConfiguration {
 
     @Bean
     public TicketValidator ticketValidator() {
+        
+        if (applicationBootstrap.getAppConfig().getCasResponseType().equals("xml")) {
+            return new Cas20ServiceTicketValidator(applicationBootstrap.getAppConfig().getCasUrlService());
+        }
         return new Cas30JsonServiceTicketValidator(applicationBootstrap.getAppConfig().getCasUrlService());
     }
 
