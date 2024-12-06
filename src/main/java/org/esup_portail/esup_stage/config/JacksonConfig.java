66515e50faf95c504e93ae99c91b3d6449ea2338
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2024-12-06 17:26:27 +0100

    fix - ajout d'une config pour Jackson

---
+++ src/main/java/org/esup_portail/esup_stage/config/JacksonConfig.java
@@ -0,0 +1,19 @@
+package org.esup_portail.esup_stage.config;
+
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class JacksonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper();
+        objectMapper.configure(SerializationFeature.FAIL_ON_EMPTY_BEANS, false); // Ignore les beans vides
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);  // Ignore les propriétés inconnues
+        return objectMapper;
+    }
+}
