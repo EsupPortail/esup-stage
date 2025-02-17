71955dc6d0b606e3215fddda1054c47ec989cb78
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2025-02-17 12:28:49 +0100

    Ajout des observateurs aux métadonnées de la signature

--- src/main/java/org/esup_portail/esup_stage/dto/MetadataSignataireDto.java
+++ src/main/java/org/esup_portail/esup_stage/dto/MetadataSignataireDto.java
@@ -1,8 +1,10 @@
 package org.esup_portail.esup_stage.dto;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.Data;
 import org.apache.logging.log4j.util.Strings;
 
+@Data
 public class MetadataSignataireDto {
     @NotNull
     private String name;
@@ -18,43 +20,7 @@ public class MetadataSignataireDto {
     @NotNull
     private int order;
 
-    public String getName() {
-        return name;
-    }
-
-    public void setName(String name) {
-        this.name = name;
-    }
-
-    public String getGivenname() {
-        return givenname;
-    }
-
-    public void setGivenname(String givenname) {
-        this.givenname = givenname;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
     public String getPhone() {
         return Strings.isEmpty(phone) ? null : phone;
-    }
-
-    public void setPhone(String phone) {
-        this.phone = phone;
-    }
-
-    public int getOrder() {
-        return order;
-    }
-
-    public void setOrder(int order) {
-        this.order = order;
     }
 }
