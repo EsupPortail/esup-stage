1bdeaa05925f6b6f717499327c7968ec46054a7d
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2025-09-02 08:45:55 +0200

    Bug #9307 - Erreur de rattachement des contacts sur la page signataire

--- src/frontend/src/app/components/convention/signataire/signataire.component.ts
+++ src/frontend/src/app/components/convention/signataire/signataire.component.ts
@@ -208,7 +208,7 @@ export class SignataireComponent implements OnInit, OnChanges, OnDestroy {
       const data = {...this.form.value};
 
       if (this.contact.id) {
-        this.contactService.update(this.contact.id, data).subscribe((response: any) => {
+          this.contactService.update(this.contact.id, data).subscribe((response: any) => {
           this.messageService.setSuccess('Contact modifié');
           this.contact = response;
           this.refreshContacts();
@@ -218,7 +218,8 @@ export class SignataireComponent implements OnInit, OnChanges, OnDestroy {
 
         //ajoute idService à l'objet contact
         data.idService = this.service.id;
-
+        //ajout idCentreGestion à l'objet contact
+        data.idCentreGestion = this.convention.centreGestion.id;
         this.contactService.create(data).subscribe((response: any) => {
           this.messageService.setSuccess('Contact créé');
           this.contact = response;
