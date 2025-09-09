7e7621b97c7f783b90808d533f53989b8a35d654
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2025-09-09 09:17:22 +0200

    story #8640 - ajout de logs supplementaire et test

--- src/main/java/org/esup_portail/esup_stage/service/signature/SignatureService.java
+++ src/main/java/org/esup_portail/esup_stage/service/signature/SignatureService.java
@@ -181,6 +181,7 @@ public class SignatureService {
     }
 
     public int upload(IdsListDto idsListDto, boolean isAvenant) {
+        logger.debug("----------------------------------| Envoi en signature de {} {} |----------------------------------", idsListDto.getIds().size(), isAvenant ? "avenants" : "conventions");
         AppSignatureEnum appSignature = signatureProperties.getAppSignatureType();
         if (appSignature == null) {
             throw new AppException(HttpStatus.BAD_REQUEST, "La signature électronique n'est pas configurée");
@@ -210,10 +211,12 @@ public class SignatureService {
             try{
                 switch (appSignature) {
                     case DOCAPOSTE:
+                        logger.debug(" - Envoi de la convention {} dans Docaposte", convention.getId());
                         docaposteClient.upload(convention, avenant);
                         break;
                     case ESUPSIGNATURE:
                     case EXTERNE:
+                        logger.debug(" - Envoi de la convention {} dans l'application de signature", convention.getId());
                         String documentId = webClient.post()
                                 .uri(signatureProperties.getWebhook().getUri() + "?" + queryParam + "=" + id)
                                 .header("Authorization", "Bearer " + signatureProperties.getWebhook().getToken())
@@ -282,6 +285,7 @@ public class SignatureService {
             }
 
         }
+        logger.debug("----------------------------------| Fin de l'envoie en signature |----------------------------------");
         return count;
     }
 
