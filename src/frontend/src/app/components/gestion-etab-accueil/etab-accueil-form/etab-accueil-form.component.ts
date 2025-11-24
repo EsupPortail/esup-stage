41906ce8cadd825dd53729bf1eb64e35948089a1
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2025-11-24 14:34:22 +0100

    story #9875 - blocage effectif de la mise à jour si l'établissement est verrouillé et pas d'affichage pour les étudiants et les enseignants des boutons de mise à jour et de verrouillage

--- src/frontend/src/app/components/gestion-etab-accueil/etab-accueil-form/etab-accueil-form.component.ts
+++ src/frontend/src/app/components/gestion-etab-accueil/etab-accueil-form/etab-accueil-form.component.ts
@@ -652,7 +652,7 @@ export class EtabAccueilFormComponent implements OnInit, OnChanges, AfterViewIni
   }
 
   canUpdateFromApi(): boolean {
-    return this.isSireneActive && this.etab?.id && this.canEdit()
+    return !(this.authService.isEtudiant() || this.authService.isEnseignant()) && this.isSireneActive && this.etab?.id && this.canEdit()
   }
 
   // Méthode pour basculer l'état du verrouillage
@@ -662,6 +662,6 @@ export class EtabAccueilFormComponent implements OnInit, OnChanges, AfterViewIni
 
   // Vérifie si le bouton de verrouillage doit être affiché
   canToggleVerrouillage(): boolean {
-    return this.isSireneActive && !!this.etab?.id && this.canEdit();
+    return !(this.authService.isEtudiant() || this.authService.isEnseignant()) && this.isSireneActive && !!this.etab?.id && this.canEdit();
   }
 }
