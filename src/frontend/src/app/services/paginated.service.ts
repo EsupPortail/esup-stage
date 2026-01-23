b5dc2581d3ecd77d0e020906d2e1adbc4178217a
Léa Raya DÉCORNOD <lea-raya.decornod@unistra.fr>
2026-01-23 12:04:04 +0100

    bug #243 - en masse, ajout d'étudiants : autocomplete sur le champ étapes

--- src/frontend/src/app/services/paginated.service.ts
+++ src/frontend/src/app/services/paginated.service.ts
@@ -1,7 +1,11 @@
 import { Observable } from "rxjs";
 
-export interface PaginatedService {
-  getPaginated(page: number, perPage: number, predicate: string, sortOrder: string, filters: string): Observable<any>;
+export interface PaginatedService<T=any> {
+  getPaginated(page: number, perPage: number, predicate: string, sortOrder: string, filters: string): Observable<PaginatedResponse<T>>;
   exportData(format: string, headers: string, predicate: string, sortOrder: string, filters: string): Observable<any>;
   getMobileTitle(row: any): string;
+}
+export type PaginatedResponse<T> = {
+  total: number;
+  data: T[];
 }
