0c7905f90d62a9a76e51f2b15d9537a3a5112dc5
Léa Raya DÉCORNOD <lea-raya.decornod@unistra.fr>
2026-04-01 15:16:49 +0200

    issue #242 - /api/etapes/apogee: tri diplomes et étapes par libellé

--- src/main/java/org/esup_portail/esup_stage/controller/EtapeController.java
+++ src/main/java/org/esup_portail/esup_stage/controller/EtapeController.java
@@ -6,10 +6,15 @@ import org.esup_portail.esup_stage.repository.EtapeRepository;
 import org.esup_portail.esup_stage.security.interceptor.Secure;
 import org.esup_portail.esup_stage.service.apogee.ApogeeService;
 import org.esup_portail.esup_stage.service.apogee.model.DiplomeEtape;
+import org.esup_portail.esup_stage.service.apogee.model.EtapeV2Apogee;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import java.text.Collator;
+import java.util.Arrays;
+import java.util.Comparator;
 
 @ApiController
 @RequestMapping("/etapes")
@@ -33,7 +38,12 @@ public class EtapeController {
     @GetMapping("/apogee")
     @Secure()
     public DiplomeEtape[] getApogeeEtapes(@RequestParam(name = "codeComposante") String codeComposante, @RequestParam(name = "codeAnnee") String codeAnnee) {
-        return apogeeService.getListDiplomeEtape(codeComposante, codeAnnee);
+        DiplomeEtape[] listDiplomeEtape = apogeeService.getListDiplomeEtape(codeComposante, codeAnnee);
+        Collator collator = Collator.getInstance();
+        Arrays.sort(listDiplomeEtape, Comparator.comparing(DiplomeEtape::getLibDiplome, collator));
+        for (DiplomeEtape diplomeEtape : listDiplomeEtape)
+            diplomeEtape.getListeEtapes().sort(Comparator.comparing(EtapeV2Apogee::getLibWebVet,collator));
+        return listDiplomeEtape;
     }
 
 }
