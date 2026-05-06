39572566c739120bd4e199d844ee0d8ca49b144c
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2026-05-06 11:22:26 +0200

    Fix supplémentaire et version 3.1.12.3

--- src/main/java/org/esup_portail/esup_stage/config/filters/CsrfCookieFilter.java
+++ src/main/java/org/esup_portail/esup_stage/config/filters/CsrfCookieFilter.java
@@ -4,7 +4,7 @@ import jakarta.servlet.FilterChain;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
-import org.springframework.security.web.server.csrf.CsrfToken;
+import org.springframework.security.web.csrf.CsrfToken;
 import org.springframework.stereotype.Component;
 import org.springframework.web.filter.OncePerRequestFilter;
 
