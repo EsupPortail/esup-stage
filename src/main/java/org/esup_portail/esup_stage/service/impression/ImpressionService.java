f73879ef49454134d4fd83e9a2f0fd97afacb168
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2026-06-04 16:28:02 +0200

    modification logs

--- src/main/java/org/esup_portail/esup_stage/service/impression/ImpressionService.java
+++ src/main/java/org/esup_portail/esup_stage/service/impression/ImpressionService.java
@@ -157,7 +157,8 @@ public class ImpressionService {
             }
             document.close();
         } catch (Exception e) {
-            e.printStackTrace();
+            logger.error("Une erreur est survenue lors de la generation du PDF {}", filename, e);
+            throw new AppException(HttpStatus.INTERNAL_SERVER_ERROR, "Erreur technique");
         } finally {
             try {
                 if (fop != null) {
@@ -166,7 +167,7 @@ public class ImpressionService {
                     file.delete();
                 }
             } catch (IOException e) {
-                e.printStackTrace();
+                logger.warn("Impossible de fermer ou supprimer le fichier temporaire du PDF {}", tempFile, e);
             }
         }
     }
