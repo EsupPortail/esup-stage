b38439217e5e62a84af2863213dab2c3144cd9b4
thouveno9 <lucas.thouvenot@univ-lorraine.fr>
2026-07-16 14:32:11 +0200

    annotation Data sur l'enseignantDTO

--- src/main/java/org/esup_portail/esup_stage/dto/EnseignantDto.java
+++ src/main/java/org/esup_portail/esup_stage/dto/EnseignantDto.java
@@ -4,8 +4,10 @@ import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
 import jakarta.validation.constraints.Size;
+import lombok.Data;
 import org.esup_portail.esup_stage.constants.ValidationPatterns;
 
+@Data
 public class EnseignantDto {
 
     @NotNull
@@ -44,86 +46,5 @@ public class EnseignantDto {
 
     @Size(max = 45)
     private String batiment;
-
-
-    public String getNom() {
-        return nom;
-    }
-
-    public void setNom(String nom) {
-        this.nom = nom;
-    }
-
-    public String getPrenom() {
-        return prenom;
-    }
-
-    public void setPrenom(String prenom) {
-        this.prenom = prenom;
-    }
-
-    public String getMail() {
-        return mail;
-    }
-
-    public void setMail(String mail) {
-        this.mail = mail;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
-    }
-
-    public String getFax() {
-        return fax;
-    }
-
-    public void setFax(String fax) {
-        this.fax = fax;
-    }
-
-    public String getTypePersonne() {
-        return typePersonne;
-    }
-
-    public void setTypePersonne(String typePersonne) {
-        this.typePersonne = typePersonne;
-    }
-
-    public String getUidEnseignant() {
-        return uidEnseignant;
-    }
-
-    public void setUidEnseignant(String uidEnseignant) {
-        this.uidEnseignant = uidEnseignant;
-    }
-
-    public String getCampus() {
-        return campus;
-    }
-
-    public void setCampus(String campus) {
-        this.campus = campus;
-    }
-
-    public String getBureau() {
-        return bureau;
-    }
-
-    public void setBureau(String bureau) {
-        this.bureau = bureau;
-    }
-
-    public String getBatiment() {
-        return batiment;
-    }
-
-    public void setBatiment(String batiment) {
-        this.batiment = batiment;
-    }
 
 }
